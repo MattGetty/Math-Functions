189053d8308cbce9585ddda62d6dce9952b31453
Matt Getty <30877976+MattGetty@users.noreply.github.com>
2017-08-21 10:21:03 -1000

    Add files via upload
    insult page using math.floor and math.random dedicated to my boi bryson

---
+++ insults.js
@@ -0,0 +1,5 @@
+var insults = ["bryson u dum", "bryson u sux", "bryson u donkey", "bryson nba2k is good", "bryson u smell bad"];
+function brysonInsults(){
+window.alert(insults[Math.floor(Math.random()*5)]);
+
+}
