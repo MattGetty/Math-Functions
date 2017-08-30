5a82f1a39f48caf472d40c72a77a513fd345ce50
Matt Getty <30877976+MattGetty@users.noreply.github.com>
2017-08-30 10:14:20 -1000

    Add files via upload
    Made 3 objects with arrays then an array that called on those other arrays.

---
+++ ObjectArrayPractice.js
@@ -0,0 +1,20 @@
+var coolObjectName = {
+  name: "Matcheewwwww",
+  age: 16,
+  grade: 12,
+  friends: ['Me', 'Myself', 'I'],
+}
+var coolObjectName2 = {
+  name: "BeandonAU",
+  age: 17,
+  grade: 1,
+  friends: ['Bryson', 'Matcheewww', 'Terruya San'],
+}
+var coolObjectName3 = {
+  name: "Bryseeeeeeeeen",
+  age: 17,
+  grade: 11,
+  friends: ['Final fantasy', 'Duckmanwoo', 'His left hand', 'his side hoe, the right hand'],
+}
+var x = [coolObjectName, coolObjectName2, coolObjectName3];
+window.alert(x[1].friends[0]);
